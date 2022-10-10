45927cf33c02a0de53bced29b2d930147f9746a9
admin <jssun0506@gmail.com>
2022-10-10 18:28:56 +0900

    준석

---
+++ src/main/java/kibwa.campus/controller/PracticeController.java
@@ -0,0 +1,22 @@
+package src.main.java.kibwa.campus.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static kibwa.campus.util.CmmUtil.nvl;
+
+
+public class PracticeController {
+
+    @RequestMapping(value="get")
+    public String get(HttpServletRequest request, ModelMap model)
+            throws Exception{
+        String name = nvl(request.getParameter("name"));
+
+        model.addAttribute("name",name);
+        return "/get";
+    }
+
+}
