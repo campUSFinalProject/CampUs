3da976b7db43a8091aceb49caebcc257e4b6edc5
JinGW99 <gunwoojin@kakao.com>
2022-10-18 19:21:54 +0900

    [JIN] 로그인 초안

---
+++ src/main/java/kibwa/campus/controller/MainController.java
@@ -0,0 +1,18 @@
+package kibwa.campus.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "cu/Main")
+    public String Main(){
+
+        log.info(this.getClass().getName() + ".MAINPAGE GO!! ");
+
+        return "/test/FinalMain";
+    }
+}
