b4d1ecfa45028d58e6938eb6c2347e58a0bc5a45
admin <jssun0506@gmail.com>
2022-10-19 16:19:52 +0900

    준석 예약(Rev) Controller, JSP 수정 해야 할 것 : HTML/CSS/JS 따와서 JSP에 집어넣기

--- src/main/java/kibwa/campus/controller/OutfieldController.java
+++ src/main/java/kibwa/campus/controller/OutfieldController.java
@@ -1,7 +1,4 @@
 package kibwa.campus.controller;
-import kibwa.campus.service.IRevService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import static kibwa.campus.util.CmmUtil.nvl;
 
-@Slf4j
+
 @Controller
-public class RevController {
-    @RequestMapping(value = "Rev")
-    public String Rev(HttpServletRequest request, ModelMap model) throws Exception {
+public class OutfieldController {
+    @RequestMapping(value = "Outfield")
+    public String Outfield(HttpServletRequest request, ModelMap model) throws Exception {
         String name = nvl(request.getParameter("name"));
         model.addAttribute("name", name);
-        return "../Rev";
+        return "../Outfield";
     }
 }
